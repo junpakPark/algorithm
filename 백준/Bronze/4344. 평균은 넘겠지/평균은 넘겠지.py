138e77d3f332506c30e59d721e0ac49c3aaeb1e9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-02 20:55:15 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -2,10 +2,10 @@ n = int(input())
 
 for _ in range(n):
   array = list(map(int, input().split()))
-  student = array.pop(0)
-  avg = sum(array)/student
+  array.pop(0)
+  avg = sum(array)/len(array)
   count = 0
-  for i in range(student):
+  for i in range(len(array)):
     if array[i] > avg:
       count += 1
-  print(format(count/student*100, '.3f')+"%")
+  print(format(count/len(array)*100, '.3f')+"%")
