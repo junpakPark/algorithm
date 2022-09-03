0353149ee496da0152d69817622333dcee2e18c9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-03 23:43:19 +0900

    [Bronze II] Title: 손익분기점, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.py
+++ 백준/Bronze/1712. 손익분기점/손익분기점.py
@@ -1,11 +1,9 @@
-import math
-
 a, b, c = map(int, input().split())
 
 if c == b :
   print(-1)
 else:
   if a/(c-b) > 0 :
-    print(math.trunc(a/(c-b)+1))
+    print(a//(c-b)+1)
   else:
     print(-1)
