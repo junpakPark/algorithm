6c6d25f9383fde0a1ccbd7392cb8b90f0a114b6e
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-06 21:12:35 +0900

    [Silver III] Title: 베르트랑 공준, Time: 664 ms, Memory: 34700 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,18 +1,21 @@
-def isPrime(a):
-  m = int(a ** 0.5)
-  if(a<2):
-    return False
-  for i in range(2, m+1):
-    if(a%i==0):
-      return False
-  return True
+import sys
+input=sys.stdin.readline
+
+a = 123456*2
+b = int(a ** 0.5)
+c = [False,False] + [True]*(a-1)
+
+for i in range(2, b + 1):
+  if c[i]:
+    for j in range(2*i, a + 1, i):
+        c[j] = False
 
 while True:
      n = int(input())
      if n != 0 :
        array = []
        for i in range(n + 1, 2 * n + 1):
-         if isPrime(i) == True:
+         if c[i] == True:
            array.append(i)
        print(len(array))
      else :
