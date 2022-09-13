c4b2e3fbc134752dd89d5ffd80fc5b3dbd9b4fcd
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-13 20:41:09 +0900

    [Silver II] Title: 좌표 압축, Time: 1824 ms, Memory: 156812 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -5,10 +5,9 @@ T = int(input())
 
 arr = list(map(int, input().split()))
 
-result = list(set(arr))
-result.sort()
+result = sorted(set(arr))
 
 dic = {result[i] : i for i in range(len(result))}
 
-for i in range(T):
-    print(dic[arr[i]], end = ' ')
+for i in arr:
+    print(dic[i], end = ' ')
