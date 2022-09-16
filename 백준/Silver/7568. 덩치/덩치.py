8d7990a7e79cdef4f440bba955b61b3e39646fc2
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-16 20:11:24 +0900

    [Silver V] Title: 덩치, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,13 +1,11 @@
-N= int(input())
+import sys
+input = sys.stdin.readline
 
-S = [list(map(int, input().split())) for i in range(N)]
+arr = [tuple(map(int, input().split())) for i in range(int(input()))]
 
-for x1, y1 in S:
-    result = 1
-    for x2, y2 in S:
+for x1, y1 in arr:
+    count = 1
+    for x2, y2 in arr:
         if x1 < x2 and y1 < y2:
-            result += 1
-
-
-    print(result, end =' ')
-    
+            count += 1
+    print(count, end =' ')
