400fd31150e6c6ec70a0277c3d9e93026dd962d9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-16 22:44:46 +0900

    [Silver IV] Title: 대칭 차집합, Time: 276 ms, Memory: 82048 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -6,7 +6,4 @@ n, m = map(int, input().split())
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
 
-union = A | B
-intersection = A & B
-
-print(len(union-intersection))
+print(len(A-B)+len(B-A))
