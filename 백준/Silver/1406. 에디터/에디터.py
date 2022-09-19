e30c90ba430d4a4c473b93debcf3ec01722831e7
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-19 20:58:59 +0900

    [Silver II] Title: 에디터, Time: 480 ms, Memory: 45360 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input().strip())
+s2= []
+
+for _ in range(int(input())):
+  command = input().split()
+  if command[0] == 'L' and s:
+    s2.append(s.pop())
+  elif command[0] == 'D' and s2:
+    s.append(s2.pop())
+  elif command[0] == 'B' and s:
+    s.pop()
+  elif command[0] == 'P' :
+    s.append(command[1])
+
+s2.reverse()
+
+
+print(*(s+s2), sep='')
