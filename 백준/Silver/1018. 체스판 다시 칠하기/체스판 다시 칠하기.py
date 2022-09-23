be99fdbf8783fe081efcd1507215c4f570f0b252
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-24 00:01:22 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = [input().strip() for _ in range(n)]
+
+answer= []
+
+for i in range(n - 8 + 1):
+  for j in range(m - 8 + 1):
+      W = 0
+      B = 0
+      for a in range(i, i + 8):
+          for b in range(j, j + 8):
+            if (a + b) % 2 == 0:
+              if arr[a][b] != 'W':
+                W += 1
+              elif arr[a][b] != 'B':
+                B += 1
+            else:
+              if arr[a][b] != 'B':
+                W += 1
+              elif arr[a][b] != 'W':
+                B += 1
+      answer.append(W)
+      answer.append(B)
+
+print(min(answer))
