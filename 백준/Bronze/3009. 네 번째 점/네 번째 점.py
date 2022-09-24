c754b054adef6c17a0d900889b3ec527931f182b
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-09-24 20:07:36 +0900

    [Bronze III] Title: 네 번째 점, Time: 88 ms, Memory: 32388 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+x = []
+y = []
+for _ in range(3):
+  a, b = map(int, input().split())
+  x.append(a)
+  y.append(b)
+
+print(Counter(x).most_common()[1][0], Counter(y).most_common()[1][0])
