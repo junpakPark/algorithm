6f6b25e2221c3f15f5f30e648c113329663983e7
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-01 23:31:31 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 32428 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+v = int(input())
+
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(v):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+def dfs(v):
+  visited[v] = 1
+  for next_com in graph[v]:
+    if visited[next_com] == 0:
+      dfs(next_com)
+
+dfs(1)
+print(sum(visited) - 1)
