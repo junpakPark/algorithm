11d2883bbb7030104d8762b99376d73769f7ac87
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-02 22:45:28 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -3,18 +3,16 @@ input = sys.stdin.readline
 
 n = int(input())
 stair = [int(input()) for _ in range(n)]
+dp = [0 for i in range(n)]
 
-if n == 1:
-  print(stair[0])
+if n <=2:
+    print(sum(stair))
 else:
-  d = [[0, 0] for _ in range(n)]
+  dp[0] = stair[0]
+  dp[1] = stair[1]
+  dp[2] = stair[2]
   
-  d[0][0] = stair[0]
-  d[0][1] = 0
-  d[1][0] = stair[1]
-  d[1][1] = stair[0] + stair[1] 
+  for i in range(3, n):
+    dp[i] = min(dp[i - 2], dp[i - 3]) + stair[i]
   
-  for i in range(2, n):
-    d[i][0] = max(d[i-2][0], d[i-2][1]) + stair[i]
-    d[i][1] = d[i-1][0] + stair[i]
-  print(max(d[n-1][0], d[n-1][1]))
+  print(sum(stair) - min(dp[i-1], dp[i-2]))
