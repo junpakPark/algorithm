d9b3ebf4805f516213df0bf50900826960f60657
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-02-01 13:08:35 +0900

    Time: 21 ms (6.37%), Space: 46.8 MB (19.56%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
+    
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
 
-        
+        List<Integer> pair = new ArrayList<>();
         int result = 0;
 
-        for (int i = 0; i < nums.length - 1; i = i + 2) {
-            result += nums[i];
+        for (int num : nums) {
+            pair.add(num);
+            if (pair.size() == 2) {
+                result += Collections.min(pair);
+                pair.clear();
+            }
         }
         return result;
     }
+
 }
