7000e589df65b7e44480d49de40cb5c5cc9d5581
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-02-20 12:05:04 +0900

    Time: 1 ms (64.33%), Space: 41.8 MB (48.53%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,14 +1,21 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> jewel = new HashMap<>();
-        for (char c : jewels.toCharArray()) {
-            jewel.put(c, 0);
+        Map<Character, Integer> stoneMap = new HashMap<>();
+
+        for (char stone : stones.toCharArray()) {
+            if (stoneMap.containsKey(stone)) {
+                stoneMap.put(stone, stoneMap.get(stone) + 1);
+                continue;
+            }
+            stoneMap.put(stone, 1);
         }
-        for (char c : stones.toCharArray()) {
-            jewel.computeIfPresent(c, (k, v) -> v + 1);
+
+        int count = 0;
+        for (char jewel : jewels.toCharArray()) {
+            if (stoneMap.containsKey(jewel)) {
+                count += stoneMap.get(jewel);
+            }
         }
-        return jewel.values().stream()
-                .mapToInt(i -> i)
-                .sum();
+        return count;
     }
 }
