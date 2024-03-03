64f986043a89847591771515d4eeea0195729ab9
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-03-03 13:51:15 +0900

    Time: 17 ms (8.14%), Space: 44.4 MB (32.30%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,25 +1,42 @@
+import java.util.Map.Entry;
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        int[][] dp = new int[K + 2][n];
-        
-        for (int[] row : dp) {
-            Arrays.fill(row, Integer.MAX_VALUE / 2);
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        for (final int[] flight : flights) {
+            int start = flight[0], end = flight[1], cost = flight[2];
+            graph.computeIfAbsent(start, k -> new HashMap<>())
+                    .put(end, cost);
         }
-        
-        dp[0][src] = 0;
 
-        
-        for (int i = 1; i <= K + 1; i++) {
-            dp[i][src] = 0; 
-            for (int[] flight : flights) {
-                int from = flight[0];
-                int to = flight[1];
-                int cost = flight[2];
-                dp[i][to] = Math.min(dp[i][to], dp[i - 1][from] + cost);
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        priorityQueue.add(new int[]{src, 0, 0});
+
+        Map<Integer, Integer> visited = new HashMap<>();
+
+        while (!priorityQueue.isEmpty()) {
+            int[] current = priorityQueue.poll();
+            int currentLocation = current[0], totalPrice = current[1], stopsMade = current[2];
+
+            if (currentLocation == dst) {
+                return totalPrice;
+            }
+            visited.put(currentLocation, stopsMade);
+
+            if (stopsMade <= K) {
+                int nextStops = stopsMade + 1;
+
+                if (graph.containsKey(currentLocation)) {
+                    for (final Entry<Integer, Integer> nextFlight : graph.get(currentLocation).entrySet()) {
+                        Integer nextLocation = nextFlight.getKey();
+                        if (!visited.containsKey(nextLocation) || nextStops < visited.get(nextLocation)) {
+                            int newTotalPrice = totalPrice + nextFlight.getValue();
+                            priorityQueue.add(new int[]{nextFlight.getKey(), newTotalPrice, nextStops});
+                        }
+                    }
+                }
             }
         }
-
-        return dp[K + 1][dst] < Integer.MAX_VALUE / 2 ? dp[K + 1][dst] : -1;
+        return -1;
     }
-
 }
