8ef91d7b313dfa60f52d8e3a6612b6346145f6c4
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-03-13 12:41:13 +0900

    Time: 1 ms (77.54%), Space: 41.2 MB (13.07%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,11 +1,16 @@
 class Solution {
     public int pivotInteger(int n) {
-        final int totalSum = (n + 1) * n / 2;
-        final int sqr = (int) Math.sqrt(totalSum);
-        
-        if(sqr * sqr == totalSum) {
-            return sqr;
+        int[] prefixSum = new int[n+1];
+
+        for (int i = 1; i < n + 1; i++) {
+            prefixSum[i] = prefixSum[i - 1] + i;
         }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (prefixSum[i] == prefixSum[n] - prefixSum[i - 1])
+                return i;
+        }
+
         return -1;
     }
 }
