2a9a66f4253ae12f86e516fe6e697b0fbcd55c19
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-03-19 14:33:00 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.74 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,67 +2,51 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        Map<Integer, Queue<Integer>> in = new HashMap<>();
-        Map<Integer, Queue<Integer>> out = new HashMap<>();
+        Map<Integer, Integer> logs = new HashMap<>();
+        Map<Integer, Integer> parkedTime = new HashMap<>();
 
-        for (String s : records) {
-            String[] record = s.split(" ");
+        for (String record : records) {
+            String[] log = record.split(" ");
 
-            int time = convertTime(record[0]);
-            int carNumber = Integer.parseInt(record[1]);
+            int time = convertTime(log[0]);
+            int carNumber = Integer.parseInt(log[1]);
 
-            if ("IN".equals(record[2])) {
-                in.computeIfAbsent(carNumber, k -> new PriorityQueue<>())
-                        .offer(time);
+            if (log[2].equals("IN")) {
+                logs.put(carNumber, time);
+                continue;
             }
-            if ("OUT".equals(record[2])) {
-                out.computeIfAbsent(carNumber, k -> new PriorityQueue<>())
-                        .offer(time);
-            }
+            int parkedDuration = time - logs.remove(carNumber);
+            parkedTime.put(carNumber, parkedTime.getOrDefault(carNumber, 0) + parkedDuration);
         }
 
-        List<Integer> carNumbers = new ArrayList<>(in.keySet());
-        carNumbers.sort(Comparator.naturalOrder());
+        for (Map.Entry<Integer, Integer> log : logs.entrySet()) {
+            int lastTime = convertTime("23:59") - log.getValue();
+
+            parkedTime.put(log.getKey(), parkedTime.getOrDefault(log.getKey(), 0) + lastTime);
+        }
+
+        List<Integer> carNumbers = new ArrayList<>(parkedTime.keySet());
+        carNumbers.sort(Integer::compare);
 
         int[] answer = new int[carNumbers.size()];
-
         for (int i = 0; i < carNumbers.size(); i++) {
-            int carNumber = carNumbers.get(i);
-
-            Queue<Integer> ins = in.get(carNumber);
-            Queue<Integer> outs = out.getOrDefault(carNumber, new LinkedList<>());
-
-            int totalTime = -fees[0];
-
-            while (!ins.isEmpty()) {
-
-                Integer outTime = outs.poll();
-                Integer inTime = ins.poll();
-
-                if (outTime == null) {
-                    outTime = convertTime("23:59");
-                }
-                if (inTime == null) {
-                    break;
-                }
-
-                totalTime += (outTime - inTime);
-            }
-
-            int targetTime = Math.max(0, totalTime);
-            int extraFee = (int) Math.ceil((float) targetTime / fees[2]) * fees[3];
-            
-            answer[i] = fees[1] + extraFee;
+            int totalTime = parkedTime.get(carNumbers.get(i));
+            answer[i] = calculateFee(fees, totalTime);
         }
 
         return answer;
     }
 
     private int convertTime(String time) {
-        String[] times = time.split(":");
-        int hour = Integer.parseInt(times[0]) * 60;
-        int minute = Integer.parseInt(times[1]);
+        String[] parts = time.split(":");
+        return Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1]);
+    }
 
-        return hour + minute;
+    private int calculateFee(int[] fees, int totalTime) {
+        if (totalTime <= fees[0]) {
+            return fees[1];
+        }
+
+        return fees[1] + (int) Math.ceil((float) (totalTime - fees[0]) / fees[2]) * fees[3];
     }
 }
