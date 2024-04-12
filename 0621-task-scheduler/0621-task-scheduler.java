c8ae94913d1205f3820d95b9fe2dccb16610e695
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-12 13:03:35 +0900

    Time: 22 ms (29.97%), Space: 45.6 MB (54.46%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,43 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] taskCounts = new int[26];
-        for (char task : tasks) {
-            taskCounts[task - 'A']++;
+        int[] frequencies = new int[26];
+        for (char c : tasks) {
+            frequencies[c - 'A']++;
         }
-        Arrays.sort(taskCounts);
-        
-        int maxFrequency = taskCounts[25];
-        int idleTime = (maxFrequency - 1) * n;
-        
-        for (int i = 24; i>=0; i--) {
-            if(idleTime <= 0) {
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> b - a);
+        for (int frequency : frequencies) {
+            if (frequency > 0) {
+                priorityQueue.add(frequency);
+            }
+        }
+
+        int result = 0;
+        while (true) {
+            int intervals = 0;
+            List<Integer> list = new ArrayList<>();
+
+            while (!priorityQueue.isEmpty()) {
+                int frequency = priorityQueue.poll();
+
+                if (intervals < n + 1) {
+                    intervals++;
+                    result++;
+
+                    if (frequency > 1) {
+                        list.add(frequency - 1);
+                    }
+                } else {
+                    list.add(frequency);
+                }
+            }
+
+            if (list.isEmpty()) {
                 break;
             }
-            idleTime -= Math.min(maxFrequency - 1, taskCounts[i]);
+            priorityQueue.addAll(list);
+            result += n + 1 - intervals;
         }
-        
-        return tasks.length + Math.max(0, idleTime);
+        return result;
     }
 }
