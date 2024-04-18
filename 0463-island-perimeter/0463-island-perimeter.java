d673e140caa38f74cf0f4b15805fb7eea32ccc01
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-18 13:27:08 +0900

    Time: 4 ms (99.58%), Space: 45.8 MB (31.17%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,40 +1,28 @@
 class Solution {
-
     private int[][] grid;
-    private int rows;
-    private int columns;
 
     public int islandPerimeter(int[][] grid) {
         this.grid = grid;
-        this.rows = grid.length;
-        this.columns = grid[0].length;
 
         int perimeter = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
                     perimeter += getPerimeter(i, j);
                 }
             }
         }
-
         return perimeter;
     }
 
     private int getPerimeter(final int i, final int j) {
-        int perimeter = 0;
-        if (i == 0 || grid[i - 1][j] == 0) {
-            perimeter++;
+        int result = 4;
+        if (i > 0 && grid[i - 1][j] == 1) {
+            result -= 2;
         }
-        if (i == rows - 1 || grid[i + 1][j] == 0) {
-            perimeter++;
+        if (j > 0 && grid[i][j - 1] == 1) {
+            result -= 2;
         }
-        if (j == 0 || grid[i][j - 1] == 0) {
-            perimeter++;
-        }
-        if (j == columns - 1 || grid[i][j + 1] == 0) {
-            perimeter++;
-        }
-        return perimeter;
+        return result;
     }
 }
