d43df92ae10d862c67d3cb3cde5e801c5e6514d4
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-18 13:41:23 +0900

    Time: 6 ms (5.12%), Space: 57.6 MB (5.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        List<Integer> sums = new ArrayList<>(List.of(nums[0]));
         for (int i = 1; i < nums.length; i++) {
-            int sum = 0;
-            if (sums.get(i - 1) > 0) {
-                sum += sums.get(i - 1);
+            if (nums[i - 1] > 0) {
+                nums[i] += nums[i - 1];
             }
-            sums.add(sum + nums[i]);
         }
-        return Collections.max(sums);
+        return Arrays.stream(nums).max().getAsInt();
     }
 }
