38089636644ac4050a2a96643ed81fc34dd243b4
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-19 13:13:22 +0900

    Time: 3 ms (86.63%), Space: 49.2 MB (75.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,36 @@
 class Solution {
+
+    private char[][] grid;
+    private int count = 0;
+
     public int numIslands(char[][] grid) {
-        int count = 0;
+        if (grid.length == 0) {
+            return 0;
+        }
+        this.grid = grid;
+
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(i, j, grid);
+                    bfs(i, j);
                     count++;
                 }
             }
         }
+
         return count;
     }
 
-    public void dfs(int i, int j, char[][] grid) {
+    private void bfs(int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
 
         grid[i][j] = '0';
-        dfs(i, j + 1, grid);
-        dfs(i, j - 1, grid);
-        dfs(i + 1, j, grid);
-        dfs(i - 1, j, grid);
+        bfs(i, j + 1);
+        bfs(i, j - 1);
+        bfs(i + 1, j);
+        bfs(i - 1, j);
     }
+
 }
