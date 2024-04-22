1ebe7bf0b169a9b9729479ccf65c20125bcf0615
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-22 13:26:56 +0900

    Time: 91 ms (68.83%), Space: 46.8 MB (75.41%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,72 +1,48 @@
+class Solution {
 
-public class Solution {
+    private final Set<String> deadEnd = new HashSet<>();
+    private final Set<String> visited = new HashSet<>();
+
     public int openLock(String[] deadends, String target) {
-        Set<String> dead = new HashSet<>(Arrays.asList(deadends));
-        Queue<Pair<String, Integer>> queue = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-        String start = "0000";
+        deadEnd.addAll(Arrays.asList(deadends));
 
-        if (dead.contains(start)) {
-            return -1;
-        }
+        return bfs(target);
+    }
 
-        queue.add(new Pair<>(start, 0));
-        visited.add(start);
+    private int bfs(String target) {
+        Queue<String> queue = new LinkedList<>();
+        visited.add("0000");
+        queue.offer("0000");
 
+        int level = 0;
         while (!queue.isEmpty()) {
-            Pair<String, Integer> p = queue.poll();
-            String state = p.getKey();
-            int depth = p.getValue();
-
-            if (state.equals(target)) {
-                return depth;
-            }
-
-            for (String nextState : getNextStates(state)) {
-                if (!visited.contains(nextState) && !dead.contains(nextState)) {
-                    visited.add(nextState);
-                    queue.add(new Pair<>(nextState, depth + 1));
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String current = queue.poll();
+                if (target.equals(current)) {
+                    return level;
+                }
+                if (deadEnd.contains(current)) {
+                    continue;
+                }
+                for (int j = 0; j < 4; j++) {
+                    for (int k = -1; k <= 1; k += 2) {
+                        char[] digits = current.toCharArray();
+                        digits[j] = (char) ((digits[j] - '0' + k + 10) % 10 + '0');
+                        String next = new String(digits);
+                        
+                        if (!visited.contains(next)) {
+                            visited.add(next);
+                            queue.offer(next);
+                        }
+                        
+                    }
                 }
             }
+            level++;
         }
-
+        
         return -1;
     }
-
-    private List<String> getNextStates(String state) {
-        List<String> nextStates = new ArrayList<>();
-        char[] stateArray = state.toCharArray();
-
-        for (int i = 0; i < 4; i++) {
-            char num = stateArray[i];
-            char num1 = num == '9' ? '0' : (char) (num + 1);
-            char num2 = num == '0' ? '9' : (char) (num - 1);
-
-            stateArray[i] = num1;
-            nextStates.add(new String(stateArray));
-            stateArray[i] = num2;
-            nextStates.add(new String(stateArray));
-            stateArray[i] = num;
-        }
-
-        return nextStates;
-    }
-
-    private static class Pair<K, V> {
-        private final K key;
-        private final V value;
-
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-    }
+    
 }
