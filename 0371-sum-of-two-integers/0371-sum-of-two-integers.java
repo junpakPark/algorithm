52d6523d367bb305a83ceb2c1094e4707a3b6e0c
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-24 13:02:30 +0900

    Time: 4 ms (100.00%), Space: 40.5 MB (22.06%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,10 +1,38 @@
 class Solution {
     public int getSum(int a, int b) {
-        while (b != 0) {
-            int carry = (a & b) << 1;  
-            a = a ^ b; 
-            b = carry;
+        String binaryStringA = toBinaryString(a);
+        String binaryStringB = toBinaryString(b);
+
+        List<Character> resultDigits = new ArrayList<>();
+        int carry = 0;
+        int sumBit;
+
+        for (int i = 0; i < 32; i++) {
+            int bit1 = getBitAt(binaryStringA, i);
+            int bit2 = getBitAt(binaryStringB, i);
+
+            int andBits = bit1 & bit2;
+            int xorBits = bit1 ^ bit2;
+            int carryAndXor = xorBits & carry;
+
+            sumBit = carry ^ xorBits;
+            carry = andBits | carryAndXor;
+            
+            resultDigits.add(0, Character.forDigit(sumBit, 2));
         }
-        return a;
+
+        String binaryResult = resultDigits.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        return Integer.parseUnsignedInt(binaryResult, 2);
+    }
+
+    private int getBitAt(final String binaryString, final int position) {
+        return Character.getNumericValue(binaryString.charAt(31 - position));
+    }
+
+    private String toBinaryString(final int number) {
+        return String.format("%32s", Integer.toBinaryString(number)).replace(' ', '0');
     }
 }
