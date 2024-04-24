eb04b7fc4a2ec1b78dced6cca30b5c3ee2b1713d
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-04-24 12:48:49 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (59.37%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,21 +1,22 @@
 class Solution {
     public int tribonacci(int n) {
         if (n == 0) {
-            return 0;
+            return n;
         }
-        if (n < 3) {
+        if (n == 1 || n == 2) {
             return 1;
         }
 
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int first = 0;
+        int second = 1;
+        int third = 1;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        for (int i = 3; i < n + 1; i++) {
+            int temp = first + second + third;
+            first = second;
+            second = third;
+            third = temp;
         }
-
-        return dp[n];
+        return third;
     }
 }
