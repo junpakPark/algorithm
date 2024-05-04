346166cab9e6f0e1546903a6f3439a364789f5bc
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-05-04 20:56:26 +0900

    Time: 16 ms (97.17%), Space: 54.6 MB (49.12%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int numBoats = 0;
+        
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--; 
+            numBoats++;
+        }
+        
+        return numBoats; 
+    }
+}
