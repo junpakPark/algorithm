61d78cb892451baa0ff9973cdb9748dbc6143895
박준현 <112045553+junpakPark@users.noreply.github.com>
2024-07-01 13:21:40 +0900

    [Silver I] Title: K 물류창고, Time: 112 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/23350. K 물류창고/K 물류창고.java
+++ 백준/Silver/23350. K 물류창고/K 물류창고.java
@@ -1,14 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
