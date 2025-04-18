111c9f36a1fada5debb5857c149aedf4dcc62dc6
박준현 <112045553+junpakPark@users.noreply.github.com>
2025-04-18 17:00:56 +0900

    [level 3] Title: 여행경로, Time: 1.09 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,25 +2,27 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] tickets) {
-        Map<String, PriorityQueue<String>> routes = new HashMap<>();
-
-        for (final String[] ticket : tickets) {
-            routes.computeIfAbsent(ticket[0], k -> new PriorityQueue<>())
-                    .add(ticket[1]);
+        final Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (String[] ticket : tickets) {
+            map.computeIfAbsent(ticket[0], k -> new PriorityQueue<>())
+                .offer(ticket[1]);
         }
-
-        Deque<String> results = new ArrayDeque<>();
-        Deque<String> seen = new ArrayDeque<>();
-
-        seen.push("ICN");
-
-        while (!seen.isEmpty()) {
-            while (routes.containsKey(seen.getFirst()) && !routes.get(seen.getFirst()).isEmpty()) {
-                seen.push(routes.get(seen.getFirst()).poll());
+        
+        final Deque<String> answer = new ArrayDeque<>();
+        final Deque<String> stack = new ArrayDeque<>();
+        stack.push("ICN");
+        
+        while(!stack.isEmpty()) {
+            final String start = stack.peek();
+            final PriorityQueue<String> ends = map.getOrDefault(start, new PriorityQueue<>());
+            
+            if (ends.isEmpty()) {
+                answer.push(stack.pop());
+                continue;
             }
-            results.addFirst(seen.pop());
+            stack.push(ends.poll());
         }
-
-        return results.toArray(new String[0]);
+        
+        return answer.toArray(new String[0]);
     }
 }
