628f43b57d4c2d8a0dbaa272ef4f26a2b88fcdb4
박준현 <112045553+junpakPark@users.noreply.github.com>
2025-04-18 14:06:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.80 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,43 @@
-
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-
-    private final Queue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
-
+    
+    private static final int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
     public int solution(int[][] maps) {
-        priorityQueue.offer(new int[]{0, 0, 1});
-
-        while (!priorityQueue.isEmpty()) {
-            int[] current = priorityQueue.poll();
-
-            if (current[0] == maps.length - 1 && current[1] == maps[0].length - 1) {
-                return current[2];
+        final int n = maps.length;
+        final int m = maps[0].length;
+        final boolean[][] visited = new boolean[n][m];
+        
+        final Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, maps[0][0]});
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()) {
+            final int[] current = queue.poll();
+            final int x = current[0];
+            final int y = current[1];
+            final int steps = current[2];
+            
+            if (x == n - 1 && y == m - 1) {
+                return steps;
             }
-            findPath(maps, current);
+            
+            for (int[] direction : directions) {
+                final int nx = x + direction[0];
+                final int ny = y + direction[1];
+                
+                if (0 > nx || nx >= n || 0 > ny || ny >= m) {
+                    continue;
+                }
+                if(visited[nx][ny] || maps[nx][ny] == 0) {
+                    continue;
+                }
+                queue.offer(new int[]{nx, ny, steps + 1});
+                visited[nx][ny] = true;
+            }
+            
         }
         return -1;
     }
-
-    private void findPath(final int[][] maps, final int[] current) {
-        final int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        int row = current[0], col = current[1], distance = current[2];
-
-        for (final int[] direction : directions) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-
-            if (isInMaps(newRow, newCol, maps) && isPath(newRow, newCol, maps)) {
-                maps[newRow][newCol] = 0;
-                priorityQueue.offer(new int[]{newRow, newCol, distance + 1});
-            }
-        }
-    }
-
-    private boolean isInMaps(final int newRow, final int newCol, final int[][] maps) {
-        int rows = maps.length - 1, cols = maps[0].length - 1;
-
-        boolean validateRow = 0 <= newRow && newRow <= rows;
-        boolean validateCol = 0 <= newCol && newCol <= cols;
-
-        return validateRow && validateCol;
-    }
-
-    private boolean isPath(final int newRow, final int newCol, final int[][] maps) {
-        return maps[newRow][newCol] == 1;
-    }
-
 }
-
