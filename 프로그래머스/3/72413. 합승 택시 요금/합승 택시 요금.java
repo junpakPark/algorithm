e28c0f6c0e35474fc67b0c81c0b5686079d75819
박준현 <112045553+junpakPark@users.noreply.github.com>
2025-04-19 22:54:48 +0900

    [level 3] Title: 합승 택시 요금, Time: 85.96 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -7,20 +7,19 @@ class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         final int[][] dist = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
+            Arrays.fill(dist[i],INF);
             dist[i][i] = 0;
         }
-        for (int[] fare : fares) {
+        
+        for (int[] fare: fares) {
             dist[fare[0]][fare[1]] = fare[2];
             dist[fare[1]][fare[0]] = fare[2];
         }
         
         for (int k = 1; k <= n; k++){
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if(dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+            for (int i = 1; i <= n; i++){
+                for (int j = 1; j <= n; j++){
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
