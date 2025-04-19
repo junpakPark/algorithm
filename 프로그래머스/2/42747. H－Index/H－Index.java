bc41dbdb8a9748bbbb4f9a4a858e16e41265a0e8
박준현 <112045553+junpakPark@users.noreply.github.com>
2025-04-19 20:35:05 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,14 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations); // 오름차순 정렬
-
-        int n = citations.length;
+        final int n = citations.length;
+        Arrays.sort(citations);
+        
         for (int i = 0; i < n; i++) {
-            if (citations[i] >= n - i) {
+            int citation = citations[i];
+            if(citation >= n - i) {
                 return n - i;
             }
         }
+        
         return 0;
     }
 }
