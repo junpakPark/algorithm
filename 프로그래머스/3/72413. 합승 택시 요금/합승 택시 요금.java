578a463e82e818571fd9f416983973e2e184c218
박준현 <112045553+junpakPark@users.noreply.github.com>
2025-04-20 09:22:45 +0900

    [level 3] Title: 합승 택시 요금, Time: 93.13 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -5,31 +5,30 @@ class Solution {
     private static final int INF = 1_000_000_000;
     
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        final int[][] dist = new int[n + 1][n + 1];
+        final int[][] graph = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i],INF);
-            dist[i][i] = 0;
+            Arrays.fill(graph[i], INF);
+            graph[i][i] = 0;
+        }
+        for (int[] fare : fares) {
+            graph[fare[0]][fare[1]] = Math.min(graph[fare[0]][fare[1]], fare[2]);
+            graph[fare[1]][fare[0]] = Math.min(graph[fare[1]][fare[0]], fare[2]);
         }
         
-        for (int[] fare: fares) {
-            dist[fare[0]][fare[1]] = fare[2];
-            dist[fare[1]][fare[0]] = fare[2];
-        }
-        
-        for (int k = 1; k <= n; k++){
-            for (int i = 1; i <= n; i++){
-                for (int j = 1; j <= n; j++){
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
             }
         }
         
         int answer = INF;
         for (int k = 1; k <= n; k++) {
-            if (dist[s][k] == INF || dist[k][a] == INF || dist[k][b] == INF) {
+            if (graph[s][k] == INF || graph[k][a] == INF || graph[k][b] == INF) {
                 continue;
             }
-            answer = Math.min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
+            answer = Math.min(answer, graph[s][k] + graph[k][a] + graph[k][b]);
         }
         return answer;
     }
